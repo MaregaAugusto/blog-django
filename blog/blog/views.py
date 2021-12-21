13423213f3d28b068942fb45eec337bf1eff9aeb
MaregaAugusto <augustomarega@gmail.com>
2021-12-20 23:45:38 -0300

    deploy

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -4,11 +4,6 @@ from django.shortcuts import render, redirect
 def Index(request):
     try:
         if request.GET['fecha'] is not None:
-            print('estoy en el index')
             return redirect('post/listarFecha/'+request.GET['fecha'])
     except :
         return render(request, 'Index.html')
-
-def Index2(request):
- 
-    return render(request, 'indexb.html')
